5c9dab1a46668347280c49c99e0af02747ab6424
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-24 23:15:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var item = {itemName: "name of the item", itemPrice: "price of the item" }
+ var item = {itemName: , itemPrice:  }
 }
 
 function viewCart() {
